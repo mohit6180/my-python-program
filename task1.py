c09ada271e141d6b2c931a53521d1ea1aaace2a4
Mohit Awana <me.mohit618@gmail.com>
2023-09-29 11:41:46 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+
+num1=input("Enter the  first number " )
+# by default input takes all argument as string
+num2=input("Enter the second number ")
+# we can chnage data type to int 
+mul=int(num1)*int(num2)
+print(mul)
